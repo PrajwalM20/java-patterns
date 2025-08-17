c5fa13f04238528ff3f0c5ffdcce581e4abb9f7f
Prajwal M <prajwalm1122@gmail.com>
2025-08-17 18:57:42 +0530

    Add files via upload

---
+++ RrightAngleTri.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class RrightAngleTri{
+    // 1. Right-Angled Triangle Pattern
+    public static void main(String[] args){
+         Scanner sc =  new Scanner(System.in);
+         int n = sc.nextInt();
+
+         for(int i=1; i<=5; i++) {
+            for(int j=1; j<=i; j++) {
+                 System.out.print("*");
+    }
+    System.out.println();
+
+     }
+
+
+    }
+}    
+
+
+
